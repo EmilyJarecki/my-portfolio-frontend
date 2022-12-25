363836bb920917b2f75b13528a49fb71cc43741b
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-24 21:45:20 -0600

    styling to project page. inserted profile image.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 function Home(props) {
   return (
-    <div className="intro">
-      <h2>I am Emily Jarecki.</h2>
-      <h1 className="title"></h1>
+    <div>
+      <div className="intro">
+        <h2>I am Emily Jarecki.</h2>
+        <h1 className="title"></h1>
+        <section className="imageSec">
+          <img className="myPic" src="https://i.imgur.com/t8x9rrO.jpeg" />
+        </section>
+      </div>
     </div>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,16 +6,14 @@ function Projects(props) {
 
   //create function to make api call
   const getProjectsData = async () => {
-    
-		//make api call and get response
+    //make api call and get response
     const response = await fetch("./projects.json");
-    
-		// turn response into javascript object
-    const data = await response.json();
-    
-		// set the projects state to the data
-    setProjects(data);
 
+    // turn response into javascript object
+    const data = await response.json();
+
+    // set the projects state to the data
+    setProjects(data);
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
@@ -25,16 +23,16 @@ function Projects(props) {
   const loaded = () => {
     return projects.map((project) => (
       <div className="projectDiv">
-        <h1>{project.name}</h1>
-        <img className="projectImg" src={project.image} />
-        <img className="projectImg" src={project.image1} />
-        <img className="projectImg" src={project.image2} />
-        <a href={project.git}>
-          <button>Github</button>
-        </a>
-        <a href={project.live}>
-          <button>Website</button>
-        </a>
+        <div className="individualProject">
+          <h1>{project.name}</h1>
+          <img className="projectImg" src={project.image} />
+          <a href={project.git}>
+            <h4>Github</h4>
+          </a>
+          <a href={project.live}>
+            <h4>Website</h4>
+          </a>
+        </div>
       </div>
     ));
   };
