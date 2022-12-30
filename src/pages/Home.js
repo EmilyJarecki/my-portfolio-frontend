9290cb9e35faa8faeb3fad0200e0d8ac0af7d6cf
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-30 15:23:55 -0600

    fixed margin of home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 function Home(props) {
   return (
-    <div>
+    <div className="Home">
       <div className="intro">
         <h2 className="iAm">I AM EMILY <span className="Jarecki"><span className="toggled">JARECKI.</span></span></h2>
         <h1 className="title"></h1>
