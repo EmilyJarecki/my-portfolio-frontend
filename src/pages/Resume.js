6644ec13b3730586808af6b942d279bb55523059
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-31 17:00:08 -0600

    opacity on navigation. tried to add animation somewhere on navigation page but didn't get anywhere

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -17,21 +17,21 @@ function Resume(props) {
     return resume.map((resume) => (
       <div>
         <div className="symbolDiv">
-            <img className="symbol" src="https://img.icons8.com/color/512/javascript.png"/>
-            <img className="symbol" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/512/external-html-5-is-a-software-solution-stack-that-defines-the-properties-and-behaviors-of-web-page-logo-color-tal-revivo.png"/>
-            <img className="symbol" src="https://img.icons8.com/stickers/512/css3.png"/>
-            <img className="symbol" src="https://img.icons8.com/fluency/512/python.png"/>
-            <img className="symbol" src="https://img.icons8.com/color/512/react-native.png"/>
-            <img className="symbol" src="https://img.icons8.com/ios/512/express-js.png"/>
-            <img className="symbol" src="https://img.icons8.com/color/512/bootstrap.png"/>
-            <img className="symbol" src="https://img.icons8.com/color/512/sass.png"/>
-            <img className="symbol" src="https://img.icons8.com/color/512/nodejs.png"/>
-            <img className="symbol" src="https://img.icons8.com/color/512/git.png"/>
-            <img className="symbol" src="https://img.icons8.com/ios-filled/512/github.png"/>
-            <img className="symbol" src="https://img.icons8.com/color/512/visual-studio-code-2019.png"/>
-            <img className="symbol" src="https://img.icons8.com/color/512/mongodb.png"/>
-            <img className="symbol" src="https://img.icons8.com/color/512/heroku.png"/>
-            <img className="symbol" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/512/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-shadow-tal-revivo.png"/>
+            <img className="symbol javascript" src="https://img.icons8.com/color/512/javascript.png"/>
+            <img className="symbol html" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/512/external-html-5-is-a-software-solution-stack-that-defines-the-properties-and-behaviors-of-web-page-logo-color-tal-revivo.png"/>
+            <img className="symbol css" src="https://img.icons8.com/stickers/512/css3.png"/>
+            <img className="symbol python" src="https://img.icons8.com/fluency/512/python.png"/>
+            <img className="symbol react" src="https://img.icons8.com/color/512/react-native.png"/>
+            <img className="symbol express" src="https://img.icons8.com/ios/512/express-js.png"/>
+            <img className="symbol bootstrap" src="https://img.icons8.com/color/512/bootstrap.png"/>
+            <img className="symbol sass" src="https://img.icons8.com/color/512/sass.png"/>
+            <img className="symbol nodejs" src="https://img.icons8.com/color/512/nodejs.png"/>
+            <img className="symbol git" src="https://img.icons8.com/color/512/git.png"/>
+            <img className="symbol github" src="https://img.icons8.com/ios-filled/512/github.png"/>
+            <img className="symbol VS" src="https://img.icons8.com/color/512/visual-studio-code-2019.png"/>
+            <img className="symbol mongodb" src="https://img.icons8.com/color/512/mongodb.png"/>
+            <img className="symbol heroku" src="https://img.icons8.com/color/512/heroku.png"/>
+            <img className="symbol netlify" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/512/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-shadow-tal-revivo.png"/>
         </div>
         <div>
             
